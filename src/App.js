f52a17741b7318f5989bfdc89332989fd3f859a4
github username <github email address>
2018-10-15 11:06:16 -0400

    working toolbar and editor using React setup

--- src/App.js
+++ src/App.js
@@ -1,28 +1,90 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ReactQuill, {Quill} from 'react-quill'; // ES6
+import 'react-quill/dist/quill.snow.css'; // ES6
+// import ImageResize from 'quill-image-resize-module';
+//
+// Quill.register('modules/imageResize', ImageResize)
+
+
 
 class App extends Component {
-  render() {
+
+  state={
+    user: 1,
+    category: 1,
+    document_id: 1,
+    delta: ""
+  }
+
+  handleChange = (content, delta, source, editor) => {
+    if (source === "user"){
+    fetch(`http://localhost:3000/documents/${this.state.document_id}`,{
+      method: "PATCH",
+      headers:{
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify({delta: editor.getContents().ops})
+      }
+    ).then(res => res.json())
+    .then(data => this.setState({delta: data.delta}))
+    }
+  }
+
+  componentDidMount(){
+    fetch(`http://localhost:3000/documents/${this.state.document_id}`)
+    .then(res=> res.json())
+    .then(data => this.setState({delta: data.delta}))
+  }
+
+    render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div id="editor">
+          <ReactQuill theme="snow"
+            onChange={this.handleChange}
+            defaultValue="hello world"
+            value={this.state.delta}
+             modules={App.modules}
+             formats={App.formats}>
+         </ReactQuill>
+        </div>
       </div>
     );
   }
 }
-
 export default App;
+
+App.modules = {
+  toolbar: [
+    ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+    ['blockquote', 'code-block'],
+
+    [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+    [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+    [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+    [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+    [{ 'direction': 'rtl' }],                         // text direction
+
+    [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+    [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+    [ 'link', 'image', 'video', 'formula' ],          // add's image support
+    [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+    [{ 'font': [] }],
+    [{ 'align': [] }],
+
+    ['clean']
+  ],
+  clipboard: {
+    // toggle to add extra line breaks when pasting HTML:
+    matchVisual: false,
+  //   imageDrop: true,
+  // imageResize: {}
+  }
+}
+App.formats = [
+  'header', 'font', 'size',
+  'bold', 'italic', 'underline', 'strike', 'blockquote',
+  'list', 'bullet', 'indent',
+  'link', 'image', 'video'
+]
---
+++ src/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+import deltas from './deltas';
+
+export default combineReducers({
+  deltas
+});
