66fbcc8ddc644c34f84ec97abe11d67fa2b22bc9
github username <github email address>
2018-11-05 10:28:43 -0500

    removed procfile and changed package.json

--- server.js
+++ server.js
@@ -1,23 +1,9 @@
 var express = require('express');
-var app = express();
+var path = require('path');
+var PORT = process.env.PORT || 5000;
 
-// set the port of our application
-// process.env.PORT lets the port be set by Heroku
-var port = process.env.PORT || 5000;
-
-// set the view engine to ejs
-app.set('view engine', 'ejs');
-
-// make express look in the public directory for assets (css/js/img)
-app.use(express.static(__dirname + '/public'));
-
-// set the home page route
-app.get('/', function(req, res) {
-
-    // ejs render automatically looks in the views folder
-    res.render('src/components/login');
-});
-
-app.listen(port, function() {
-    console.log('Our app is running on http://localhost:' + port);
-});
+express().use(express.static(path.join(__dirname, 'public')))
+	.set('views', path.join(__dirname, 'views'))
+	.set('view engine', 'ejs')
+	.get('/', (req, res) => res.render('pages/index'))
+	.listen(PORT, () => console.log(`Listening on ${ PORT }`));
